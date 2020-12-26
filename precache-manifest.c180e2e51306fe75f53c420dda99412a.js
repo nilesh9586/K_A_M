a736f9cb5f457b7e4dd079a3b0842686445a7c40
nilesh9586 <nilesh16mcg34@gmail.com>
2020-12-26 12:46:00 +0530

    Updates

---
+++ precache-manifest.c180e2e51306fe75f53c420dda99412a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "2779b1abbf40b9f3ccda",
+    "url": "/K_A_M/static/css/main.d7d6d321.chunk.css"
+  },
+  {
+    "revision": "2779b1abbf40b9f3ccda",
+    "url": "/K_A_M/static/js/main.97b1d1d5.chunk.js"
+  },
+  {
+    "revision": "63adb921d6807497799e",
+    "url": "/K_A_M/static/js/runtime~main.b28365d1.js"
+  },
+  {
+    "revision": "62730646cb1c0f761d75",
+    "url": "/K_A_M/static/css/2.48f7b6a0.chunk.css"
+  },
+  {
+    "revision": "62730646cb1c0f761d75",
+    "url": "/K_A_M/static/js/2.919c2537.chunk.js"
+  },
+  {
+    "revision": "5145fb8eb621e5cefef2",
+    "url": "/K_A_M/static/js/3.ce160b2c.chunk.js"
+  },
+  {
+    "revision": "219d56ac5577d4bee0568c8f979f9b5a",
+    "url": "/K_A_M/static/media/Motorcycle1.219d56ac.jpg"
+  },
+  {
+    "revision": "796c65100252e8a413155a3297c97b7b",
+    "url": "/K_A_M/static/media/Motorcycle3.796c6510.jpg"
+  },
+  {
+    "revision": "e6084e15888d5815ab1d4dcc6a716803",
+    "url": "/K_A_M/static/media/Motorcycle5.e6084e15.jpeg"
+  },
+  {
+    "revision": "b7cd2d29af6741fa8f0bb90e43eb7659",
+    "url": "/K_A_M/static/media/Motorcycle6.b7cd2d29.jpeg"
+  },
+  {
+    "revision": "624536bafae26e66f84c04e942c88352",
+    "url": "/K_A_M/static/media/Motorcycle7.624536ba.jpeg"
+  },
+  {
+    "revision": "30c9a3a312d3503b910a2aa18181c889",
+    "url": "/K_A_M/static/media/sctooter1.30c9a3a3.jpeg"
+  },
+  {
+    "revision": "b234fd1eeb04e1299a88eafb6df9968a",
+    "url": "/K_A_M/static/media/Chainrings.b234fd1e.jpg"
+  },
+  {
+    "revision": "9540b943370bc9a29c0c45611a9c50cb",
+    "url": "/K_A_M/index.html"
+  }
+];
